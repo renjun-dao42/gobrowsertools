b7d5c427a8f698a8bca857b3a25480b79e3142d3
Clacky <develop@clacky.ai>
2025-05-20 23:29:34 -0400

    update

--- pkg/browser/manager_test.go
+++ pkg/browser/manager_test.go
@@ -1,1 +1,29 @@
 package browser
+
+import (
+	"context"
+	"fmt"
+	"testing"
+)
+
+func TestBrowserManager_OpenTab(t *testing.T) {
+	manager := NewBrowserManager()
+	
+	if !manager.IsInstalled() {
+		fmt.Println("Browser is not installed, skipping test")
+	}
+	
+	browser, err := manager.GetOrCreateBrowser()
+	if err != nil {
+		t.Fatalf("GetOrCreateBrowser() error = %v", err)
+	}
+	
+	ctx := context.Background()
+	err = browser.OpenTab(ctx, "https://www.baidu.com")
+	if err != nil {
+		t.Errorf("OpenTab() error = %v", err)
+	}
+	
+	// 清理资源
+	browser.Close()
+}
